cf1a7e52f10c18052858a307a4755f25e90a8a30
SainezKimutai <sainezkimutai@gmail.com>
2019-09-04 16:30:13 +0300

    user crud endpoints

--- erp/erp/api/users/index.js
+++ erp/erp/api/users/index.js
@@ -7,7 +7,7 @@ router.get('/getAll', controller.getAll);
 router.get('/getOne/:id', controller.getOne);
 router.post('/login', controller.login);
 router.post('/register', controller.create);
-router.post('/update/:id', controller.update);
+router.put('/update/:id', controller.update);
 router.delete('/delete/:id', controller.delete);
 
 module.exports = router;
