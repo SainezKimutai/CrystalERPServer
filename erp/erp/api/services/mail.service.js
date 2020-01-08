1aacbfe00bfc0ea9c1d254ce6c60f276358b2234
Sainezkimutai <sainezkimutai@gmail.com>
2020-01-08 14:47:28 +0300

    changes

--- erp/erp/api/services/mail.service.js
+++ erp/erp/api/services/mail.service.js
@@ -7,13 +7,16 @@ function sendInvoiceToClient(reqParam){
     return new Promise((resolve, reject)=>{
 
         let options = {
+            host: "crystaltourskenya.com",
+            port: 465,
             auth: {
-                api_user: 'SainezKimutai', // Sendgrid username
-                api_key: 'saineZ@13' // Sendgrid password
+                user: 'erp@crystaltourskenya.com',
+                pass: 'Globaldistruption@2020'
             }
         }
 
-        let transporter = nodemailer.createTransport(sgTransport(options));
+        let transporter = nodemailer.createTransport((options));
+
         let email = {
             from: reqParam.sender,
             to: reqParam.reciever,
@@ -41,14 +44,16 @@ function sendInvoiceToClient(reqParam){
  function sendMailToClient(reqParam){
     return new Promise((resolve, reject)=>{
 
-        let options = {
-            auth: {
-                api_user: 'SainezKimutai', // Sendgrid username
-                api_key: 'saineZ@13' // Sendgrid password
-            }
-        }
+      let options = {
+          host: "crystaltourskenya.com",
+          port: 465,
+          auth: {
+              user: 'erp@crystaltourskenya.com',
+              pass: 'Globaldistruption@2020'
+          }
+      }
 
-        let transporter = nodemailer.createTransport(sgTransport(options));
+        let transporter = nodemailer.createTransport((options));
 
         let email = {
             from: reqParam.sender,
@@ -68,14 +73,16 @@ function sendInvoiceToClient(reqParam){
 function inviteUser(reqParam){
     return new Promise((resolve, reject)=>{
 
-        let options = {
-            auth: {
-                api_user: 'SainezKimutai', // Sendgrid username
-                api_key: 'saineZ@13' // Sendgrid password
-            }
-        }
+      let options = {
+          host: "crystaltourskenya.com",
+          port: 465,
+          auth: {
+              user: 'erp@crystaltourskenya.com',
+              pass: 'Globaldistruption@2020'
+          }
+      }
 
-        let transporter = nodemailer.createTransport(sgTransport(options));
+        let transporter = nodemailer.createTransport((options));
 
         let email = {
             from: reqParam.sender,
@@ -96,3 +103,38 @@ function inviteUser(reqParam){
 
 
  module.exports = { sendMailToClient, inviteUser, sendInvoiceToClient }
+
+
+
+
+
+
+
+ //
+ // return new Promise((resolve, reject)=>{
+ //
+ //        let options = {
+ //            host: "smtp.gmail.com",
+ //            port: 465,
+ //            auth: {
+ //                user: 'muindegeofrey@gmail.com',
+ //                pass: 'Gracemakau2019'
+ //            }
+ //        }
+ //
+ //        let transporter = nodemailer.createTransport((options));
+ //
+ //        let email = {
+ //            from: reqParam.sender,
+ //            to: reqParam.reciever,
+ //            subject: "Invitation To Aidy",
+ //            text: 'Hello',
+ //            html: 'You have been invited to Aidy. Please click on the link below to register:<br><br> <button style="margin-left:70px;border:none;padding:7px;border-radius:5px;background-color:teal;color:white;"><a style="color:white;font-size:14px;font-family:verdana" href="http://localhost/register/' + reqParam.reciever + '/' + reqParam.token + '">Invitation</a></button>'
+ //        };
+ //
+ //        transporter.sendMail(email, function (err, data) {
+ //            !err ? resolve() : reject('Something went wrong');
+ //         });
+ //
+ //
+ //    });
